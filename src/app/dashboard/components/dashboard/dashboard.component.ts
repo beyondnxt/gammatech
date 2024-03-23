86a1dba243979c657db47565a8b3a4b8816a309b
beyondnxt <contact@beyondnxt.com>
2024-03-23 12:28:42 +0530

    Websocket changes

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as data from './dashboard-data';
+import { WebSocketService } from 'src/app/providers/core/web-socket.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,15 +8,49 @@ import * as data from './dashboard-data';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  constructor(private websocketService: WebSocketService) { }
+
 
   tableHeaders = data.tableHeaders;
   tableValues = data.tableValues;
 
   ngOnInit(){
     this.getAllDetails();
+    // this.initializeSocketConnection();
+    this.receiveSocketResponse();
   }
 
   getAllDetails(){
     
   }
+
+  ngOnDestroy() {
+    // this.disconnectSocket();
+   }
+  
+   // Initializes socket connection
+  //  initializeSocketConnection() {
+  //   this.websocketService.connectSocket('message');
+  //  }
+  
+   // Receives response from socket connection 
+   receiveSocketResponse() {
+    this.websocketService.receiveStatus().subscribe(
+      {
+        next: (res) => {
+         console.log('res::',res);
+        },
+        error: (err) => {
+          console.log(err);
+         },
+        complete: () => {
+       }
+      }
+    );
+   }
+  
+   // Disconnects socket connection
+  //  disconnectSocket() {
+  //   this.websocketService.disconnectSocket();
+  //  }
 }
